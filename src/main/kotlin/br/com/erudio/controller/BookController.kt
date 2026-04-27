915f8dbe6bb57bb6aa5789b413d59bec3c655227
danielv <danielv@ciandt.com>
2026-04-27 16:51:25 -0300

    second commit

--- src/main/kotlin/br/com/erudio/controller/BookController.kt
+++ src/main/kotlin/br/com/erudio/controller/BookController.kt
@@ -1,6 +1,7 @@
 package br.com.erudio.controller
 
 import br.com.erudio.model.Book
+import br.com.erudio.proxy.CambioProxy
 import br.com.erudio.repository.BookRepository
 import br.com.erudio.response.Cambio
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,6 +24,9 @@ class BookController {
     @Autowired
     private lateinit var repository: BookRepository
 
+    @Autowired
+    private lateinit var proxy: CambioProxy
+
     @GetMapping(value = ["/{id}/{currency}"])
     fun findBook(
         @PathVariable("id") id: Long,
@@ -31,24 +35,43 @@ class BookController {
         val book = repository.findById(id)
             .orElseThrow {RuntimeException("Book not found")}
 
-        val params = HashMap<String, String>()
-        params["amount"] = book.price.toString()
-        params["from"] = "USD"
-        params["to"] = currency
-
-        val response = RestTemplate()
-            .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
-                Cambio::class.java,
-                params
-                )
-
-        val cambio = response.body
+        val cambio = proxy.getCambio(book.price, "USD", currency)
 
         val port = environment.getProperty("local.server.port")
-        book.environment = port
+        book.environment = "$port FEIGN"
         book.currency = currency
         book.price = cambio!!.convertedValue
 
         return book
     }
 }
+
+//REST TEMPLATE
+//@GetMapping(value = ["/{id}/{currency}"])
+//fun findBook(
+//    @PathVariable("id") id: Long,
+//    @PathVariable("currency") currency: String
+//): Book? {
+//    val book = repository.findById(id)
+//        .orElseThrow {RuntimeException("Book not found")}
+//
+//    val params = HashMap<String, String>()
+//    params["amount"] = book.price.toString()
+//    params["from"] = "USD"
+//    params["to"] = currency
+//
+//    val response = RestTemplate()
+//        .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+//            Cambio::class.java,
+//            params
+//        )
+//
+//    val cambio = response.body
+//
+//    val port = environment.getProperty("local.server.port")
+//    book.environment = port
+//    book.currency = currency
+//    book.price = cambio!!.convertedValue
+//
+//    return book
+//}
